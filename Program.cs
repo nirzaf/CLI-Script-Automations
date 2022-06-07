5a6de655333a6f9b60bfe13395726de7bd5f2cdf
nirzaf <mfmfazrin1986@gmail.com>
2022-06-07 18:49:34 +0530

    struct Currency     {          public uint Dollars;          public ushort Cents;

--- Program.cs
+++ Program.cs
@@ -7,11 +7,46 @@ namespace CLI_Script_Automations
     {
         static async Task Main(string[] args)
         {
+            var currency = new Currency(34,2234);
+
+            Console.WriteLine(currency.ToString());
+            Console.WriteLine(currency);
+
             var cliResponse = await Cli.Wrap("dotnet")
                 .WithArguments( new[] {"--version" })
                 .ExecuteBufferedAsync();
             Console.WriteLine(cliResponse.StandardOutput);
 
+           //Currency c1;
+
+           // Console.WriteLine(c1.ToString());
+
         }
+    }
+
+    struct Currency
+    {
+         public uint Dollars;
+         public ushort Cents;
+         public Currency(uint dollars, ushort cents)
+         {
+             Dollars = dollars;
+             Cents = cents;
+         }
+         public override string ToString() => $"${Dollars}.{Cents,2:00}";
+         public static string GetCurrencyUnit() => "Dollar";
+         public static explicit operator Currency (float value)
+         {
+             checked
+             {
+                uint dollars = Convert.ToUInt16(value);
+                ushort cents = (ushort)((ushort)value - ((ushort)dollars *100));
+                return new Currency(dollars, cents);
+             }
+         }
+
+         public static implicit operator float (Currency value) => value.Dollars + (value.Cents / 100.0f);
+         public static implicit operator Currency (uint value) => new(value, 0);
+         public static implicit operator uint (Currency value) => value.Dollars;
     }
 }
