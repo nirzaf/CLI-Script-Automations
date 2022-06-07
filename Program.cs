5b8f65a17a4bafd1c8e8bf50795ba16240b592ee
nirzaf <mfmfazrin1986@gmail.com>
2022-06-07 15:55:44 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using CliWrap;
+using CliWrap.Buffered;
+
+namespace CLI_Script_Automations
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var cliResponse = await Cli.Wrap("dotnet")
+                .WithArguments( new[] {"--version" })
+                .ExecuteBufferedAsync();
+            Console.WriteLine(cliResponse.StandardOutput);
+
+        }
+    }
+}
