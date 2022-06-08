91d1ea2ae228ac60e11edafba85cc05c15c25c2d
nirzaf <mfmfazrin1986@gmail.com>
2022-06-08 14:30:28 +0530

    pushed by mfmfazrin1986@gmail.com at Wednesday, June 8, 2022, 02:30:28 PM

--- .history/demo_20220608143002.cs
+++ .history/demo_20220608143002.cs
@@ -2,13 +2,13 @@
 for(int i=1; i<=100; i++)
 {
 
-    if(i%3 == 0 && i%5==0 && i%) 
+    if(i%3 == 0 && i%5==0) 
         Console.WriteLine("Fizz Buzz");
     else if(i%5==0) 
         Console.WriteLine("Buzz");
     else if(i%3 == 0) 
         Console.WriteLine("Fizz");
-    else if(i%3 == 0) 
+            else if(i%3 == 0) 
         Console.WriteLine("Fizz");
     else
         Console.WriteLine(i.ToString());
