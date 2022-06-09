3fb31464cc91c4e9fed3848d0b820115240bc3a1
nirzaf <mfmfazrin1986@gmail.com>
2022-06-09 20:28:00 +0530

    pushed by mfmfazrin1986@gmail.com at Thursday, June 9, 2022, 08:27:59 PM

--- .history/Program_20220609202619.cs
+++ .history/Program_20220609202619.cs
@@ -8,7 +8,7 @@ namespace CLI_Script_Automations
     {
         static async Task Main(string[] args)
         {
-            Services.PrintSum(1, 3);
+            Services.PrintSum(1, 100);
         }
     }
 }
--- .history/Services_20220609202529.cs
+++ .history/Services_20220609202529.cs
@@ -1,6 +1,6 @@
-public static class Services
+public class Services
 {
-   public static void PrintSum(int a, int b)
+   public void PrintSum(int a, int b)
     {
         int sum = 0;
         for (int i = a; i <= b; i++)
