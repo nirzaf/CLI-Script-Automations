7f4e921607016fd3359bc1d56f004714276bf684
nirzaf <mfmfazrin1986@gmail.com>
2022-06-10 08:17:38 +0530

    pushed by mfmfazrin1986@gmail.com at Friday, June 10, 2022, 08:17:38 AM

---
+++ .history/Team_20220610081702.cs
@@ -0,0 +1,40 @@
+   public class Team {
+        public string teamName {get;set;}
+        public int noOfPlayers {get;set;}
+        
+        public void Team(string team, int players)
+        {
+            teamName = team;
+            noOfPlayers = players; 
+        }
+        
+        public void AddPlayer(int count)
+        {
+            noOfPlayers += count;
+        }
+        
+        public bool RemovePlayer(int count)
+        {
+            if(noOfPlayers<=0) 
+                return false;
+            else 
+            {
+                noOfPlayers -= count;
+                return true;
+            }
+        }
+    }
+
+    public class Subteam: Team {
+        
+        public void Subteam(string teanName, int noOfPlayer)
+        {
+            base.Team(string teanName, int noOfPlayer);
+        }
+        
+        public void ChangeTeamName(string name)
+        {
+            base.teamName = name;
+        }
+        
+    }
