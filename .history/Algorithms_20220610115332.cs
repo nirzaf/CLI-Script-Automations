b5c0b5da77da637e52d7b8da1795440eb051e309
nirzaf <mfmfazrin1986@gmail.com>
2022-06-10 11:54:00 +0530

    pushed by mfmfazrin1986@gmail.com at Friday, June 10, 2022, 11:53:59 AM

--- .history/Algorithms_20220610115332.cs
+++ .history/Algorithms_20220610115332.cs
@@ -2,5 +2,5 @@ public static class Algorithms
 {
    // A left rotation operation on an array shifts each of the array's elements  unit to the left. For example, if  left rotations are performed on array , then the array would become .
    // Given an array  of  integers and a number, , perform  left rotations on the array. Return the updated array to be printed as a single line of space-separated integers.
-   
+
 }
