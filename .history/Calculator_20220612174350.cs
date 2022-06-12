ff74c4b52df5fec2fbebf49bc566ddcc681c5024
nirzaf <mfmfazrin1986@gmail.com>
2022-06-12 17:44:11 +0530

    pushed by mfmfazrin1986@gmail.com at Sunday, June 12, 2022, 05:44:11 PM

---
+++ .history/Calculator_20220612174350.cs
@@ -0,0 +1,32 @@
+﻿
+namespace CLI_Script_Automations
+{
+    public static class Calculator
+    {
+
+        public static double Add(double num1, double num2)
+        {
+            return num1 + num2;
+        }
+
+        public static double Subtract(double num1, double num2)
+        { 
+          return num1-num2;   
+        }
+
+        //get the input from the user and print the multipl
+
+        public static string GetMyFullName(string firstName, string lastName)
+        {
+            var fullName = "Welcome ! " + firstName + " " + lastName + "...";
+
+            return fullName;
+        }
+
+        public static void PrintMyName(string name)
+        { 
+            Console.WriteLine("Hi your name is :" +  name);    
+        }
+
+    }
+}
